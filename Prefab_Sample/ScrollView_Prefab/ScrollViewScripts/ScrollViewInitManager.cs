5b9d484a77ebd9221576b2b44d883d4167a7367e
UnderSakura <95558098+UnderSakura@users.noreply.github.com>
2022-11-09 17:19:06 +0800

    Change ScrollViewInitManager file to perfect.

--- Prefab_Sample/ScrollView_Prefab/ScrollViewScripts/ScrollViewInitManager.cs
+++ Prefab_Sample/ScrollView_Prefab/ScrollViewScripts/ScrollViewInitManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -22,30 +24,49 @@ public class ScrollViewInitManager : MonoBehaviour
         InitCardsInList();
     }
 
-    void InitCardsInList() {
+    /**
+     * 点击Button生成滑片，请调用这个方法
+     */
+    public void InitCardsInList() {
         GameObject[] gameObjects = new GameObject[contents.Count];
 
         var tmp_GameObject = new GameObject("EmptyGameObject");
-        
-        var parentObject = tmp_GameObject.transform;
+        tmp_GameObject.transform.position = new Vector3(0, GameObject.Find("SV_Canvas").GetComponent<RectTransform>().sizeDelta.y, 0);
+        tmp_GameObject.transform.SetParent(GameObject.Find("SV_Canvas").transform);
+        var parentObject = tmp_GameObject.AddComponent<RectTransform>();
+        parentObject.anchoredPosition = Vector2.zero;
+        tmp_GameObject.GetComponent<RectTransform>().sizeDelta = new Vector2((InitLength * contents.Count + InitLength / 4), 600f);
 
         for (int index = 0; index < contents.Count; index++) {
-            
-            var current_Trans_x = parentObject.transform.position.x + InitLength;
-            parentObject.transform.position = new Vector3(current_Trans_x, parentObject.transform.position.y, parentObject.transform.position.z);
-            var thisContent = Instantiate(UIContent_Prefab, parentObject.transform);
-            gameObjects[index] = thisContent;
-            thisContent.GetComponentInChildren<Text>().text = contents[index].ContentText;
-
-            thisContent.transform.SetParent(tmp_GameObject.transform);
+            //Debug.Log(parentObject.anchoredPosition.x);
+            if (index == 0) { parentObject.anchoredPosition = InitCurrentCardInWorld(parentObject, index, gameObjects, true, tmp_GameObject); }
+            else {
+                parentObject.anchoredPosition = InitCurrentCardInWorld(parentObject, index, gameObjects, false, tmp_GameObject);
+            } 
         }
-        tmp_GameObject.transform.SetParent(GameObject.Find("Canvas").transform);
 
-        var rectTrans = tmp_GameObject.AddComponent<RectTransform>();
         var SetContent = this.GetComponentInChildren<ScrollRect>();
-        rectTrans.anchorMin = gameObjects[0].GetComponent<RectTransform>().anchorMin;
-        rectTrans.anchorMax = gameObjects[contents.Count - 1].GetComponent<RectTransform>().anchorMax;
-        rectTrans.pivot.Equals(gameObjects[0].GetComponent<RectTransform>().pivot);
         SetContent.content = tmp_GameObject.GetComponent<RectTransform>();
+
+        //Debug.Log(tmp_GameObject.GetComponent<RectTransform>().sizeDelta.x / 2f);
+    }
+
+    Vector2 InitCurrentCardInWorld(RectTransform parentObject, int index, GameObject[] gameObjects, bool isIndexO, GameObject parentGameObject) {
+        float current_Trans_x = InitLength / 2f;
+        if (!isIndexO) {
+            current_Trans_x = parentObject.anchoredPosition.x + InitLength;
+        }
+        parentObject.anchoredPosition = new Vector2(current_Trans_x, parentObject.anchoredPosition.y);
+        var thisContent = Instantiate(UIContent_Prefab);
+        gameObjects[index] = thisContent;
+        thisContent.GetComponentInChildren<Text>().text = contents[index].ContentText;
+        var tmp_rectTransform = thisContent.GetComponent<RectTransform>();
+        
+        tmp_rectTransform.gameObject.transform.SetParent(GameObject.Find("EmptyGameObject").transform);
+        tmp_rectTransform.anchoredPosition = new Vector2((parentObject.anchoredPosition.x - (parentGameObject.GetComponent<RectTransform>().sizeDelta.x / 2f)), parentObject.anchoredPosition.y);
+        Debug.Log("减下的固定长度父物体长度的一半：" + (parentGameObject.GetComponent<RectTransform>().sizeDelta.x / 2f));
+        Debug.Log((parentObject.anchoredPosition.x - (parentGameObject.GetComponent<RectTransform>().sizeDelta.x / 2f)));
+
+        return parentObject.anchoredPosition;
     }
 }
